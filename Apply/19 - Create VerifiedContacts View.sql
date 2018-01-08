64ca855ce52c5c5b74e0cd4684838b07acbb9aac
DBA-Andy <33224955+DBA-Andy@users.noreply.github.com>
2018-01-08 17:16:31 -0500

    View Script Added

---
+++ Apply/19 - Create VerifiedContacts View.sql
@@ -0,0 +1,22 @@
+USE AddressBook;
+
+IF EXISTS (select 1 from sys.views where [Name] = 'VerifiedContacts')
+BEGIN
+   DROP VIEW dbo.VerifiedContacts;
+END;
+
+GO
+
+CREATE VIEW dbo.VerifiedContacts
+WITH SCHEMABINDING
+AS
+SELECT C.ContactID, C.FirstName, C.LastName, C.DateOfBirth, 
+CASE C.AllowContactByPhone 
+   WHEN 1 THEN 'Yes'
+   ELSE 'No'
+END
+AS AllowContactbyPhone, CVD.DrivingLicenseNumber, CVD.PassportNumber
+FROM dbo.Contacts C
+   INNER JOIN dbo.ContactVerificationDetails CVD
+      ON c.ContactId = CVD.ContactID
+WHERE CVD.ContactVerified = 1;
